7b6e6ccf1bce81766eee2dae7f3541dc6e6b03de
ub3rdevice <brigada1111@mail.ru>
2024-10-10 23:50:33 +0400

    implemented weapon switcher via keys & scroll

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -1,18 +1,88 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponSwitcher : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] int currentWeapon = 0;
+
+
     void Start()
     {
-        
+        SetWeaponActive();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        int previousWeapon = currentWeapon;
+
+        ProcessKeyInput();
+        ProcessScroll();
+
+        if(previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    void ProcessKeyInput()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            currentWeapon = 0;
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            currentWeapon = 1;
+        }
+        else if (Input.GetKey(KeyCode.Alpha3))
+        {
+            currentWeapon = 2;
+        }
+    }
+
+    void ProcessScroll()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if(currentWeapon >= transform.childCount - 1)
+            {
+                currentWeapon = 0;
+            }
+            else
+            {
+                currentWeapon++;
+            }
+        }
+        if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if(currentWeapon <= 0)
+            {
+                currentWeapon = transform.childCount -1;
+            }
+            else
+            {
+                currentWeapon--;
+            }
+        }
+    }
+
+    void SetWeaponActive()
+    {
+        int weaponIndex = 0;
+
+        foreach(Transform weapon in transform)
+        {
+            if(weaponIndex == currentWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            weaponIndex++;
+        }
     }
 }

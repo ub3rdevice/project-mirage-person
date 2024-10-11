91ef8db91c649714d5668b4083d2cebe9c816ac3
ub3rdevice <brigada1111@mail.ru>
2024-10-12 00:04:36 +0400

    added proper models for pickups

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -7,13 +7,23 @@ public class AmmoPickup : MonoBehaviour
     [SerializeField] int ammoAmount = 5;
     [SerializeField] AmmoType ammoType;
 
-    void OnCollisionEnter(Collision other) 
+
+    void OnTriggerEnter(Collider other) 
     {
         if(other.gameObject.tag == "Player")
         {
             FindObjectOfType<Ammo>().AddCurrentAmmo(ammoType, ammoAmount);
             Destroy(gameObject);
         }
-        
     }
+
+    // void OnCollisionEnter(Collision other) 
+    // {
+    //     if(other.gameObject.tag == "Player")
+    //     {
+    //         FindObjectOfType<Ammo>().AddCurrentAmmo(ammoType, ammoAmount);
+    //         Destroy(gameObject);
+    //     }
+        
+    // }
 }
